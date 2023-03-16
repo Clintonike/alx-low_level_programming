30bb2dda8be96a513931bfdf908ed7b896cd442a
clintonike <driplordy696@gmail.com>
2023-03-16 15:06:42 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <101-quote.c>
+#include <101-quote.c>
+
+/**
+ * main - Entry point
+ *Return: Always 1 (success)
+ */
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful" - Dora Korpar, 2015-10-19);
+	return (1);
+}
