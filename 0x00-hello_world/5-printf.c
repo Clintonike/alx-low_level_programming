bf1545bf5a5fb7aa96106753d8198529d158bea2
clintonike <driplordy696@gmail.com>
2023-03-17 14:03:48 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<5-printf.c>
+#include <stdio.h>
+
 /**
- * main entry - point
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
-
