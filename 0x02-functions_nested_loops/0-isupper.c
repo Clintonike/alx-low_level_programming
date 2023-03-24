e6c49ce4fe5e9fecee53d6d1b704cb01f91ebd7f
clintonike <driplordy696@gmail.com>
2023-03-24 13:37:47 -0700

    isupper

--- 0x02-functions_nested_loops/0-isupper.c
+++ 0x02-functions_nested_loops/0-isupper.c
@@ -2,14 +2,15 @@
 
 /**
 * _isupper - checks for lowercase character
-* @c: The character to be checked
-* Return: 1 for uppercase character or 0 for anything else
+* @c: char to check
+*
+* Return: 0 or 1
 */
 
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-{return (1);
-}
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
 return (0);
 }
